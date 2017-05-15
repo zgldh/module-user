5a87556cd73d02009372558ec004709216f1be0f
zgldh <zgldh@hotmail.com>
2017-05-15 21:57:14 +0800

    first commit

---
+++ src/UserServiceProvider.php
@@ -0,0 +1,31 @@
+<?php namespace zgldh\Scaffold;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Created by PhpStorm.
+ * User: zhangwb-pc
+ * Date: 11/14/2016
+ * Time: 2:00 PM
+ */
+class UserServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
